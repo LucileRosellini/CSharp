08d1cc9f634b30cee0d96fd48e1a6877317e2998
Lucile ROSELLINI <l.rosellini@cerberis.com>
2021-02-03 23:16:50 +0100

    test sans interface

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HelloWorld;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+
+        {
+            Date TestDate = new Date();
+            DateTime CompareDate = new DateTime(2008, 11, 1, 13, 45, 10);
+
+            Assert.AreEqual(TestDate.GetTime(), CompareDate);
+      
+        }
+    }
+}
